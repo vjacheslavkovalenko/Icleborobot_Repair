f4167ecc54628bb705af867d0bf301d7977b1920
Vyacheslav Kovalenko <vjacheslavkovalenko@gmail.com>
2023-12-12 19:10:18 +0300

    Commit N1. First activate WebView.

--- app/src/main/java/by/icleborobot_repair/MainActivity.kt
+++ app/src/main/java/by/icleborobot_repair/MainActivity.kt
@@ -2,10 +2,18 @@ package by.icleborobot_repair
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var urlWeb = getString(R.string.website_url)
+        val webView = findViewById<WebView>(R.id.xml_webview)
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl(urlWeb)
     }
 }
