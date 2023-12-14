78b81caaf492175cb84c198441f595b32a058773
vjacheslavkovalenko <vjacheslavkovalenko@gmail.com>
2023-12-14 13:18:24 +0300

    Commit N 7. Change theme (no Bar). And go otkat.

--- app/src/main/java/by/icleborobot_repair/MainActivity.kt
+++ app/src/main/java/by/icleborobot_repair/MainActivity.kt
@@ -2,32 +2,22 @@ package by.icleborobot_repair
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var webView: WebView
-    var urlWeb = getString(R.string.website_url)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var urlWeb = getString(R.string.website_url)
+        val webView = findViewById<WebView>(R.id.xml_webview)
 
-        //val webView = findViewById<WebView>(R.id.xml_webview)
-
-        webView = findViewById(R.id.xml_webview)
         webView.webViewClient = WebViewClient()
         webView.loadUrl(urlWeb)
-    }
-
-    override fun onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
     }
 
 }
