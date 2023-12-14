bcd011c70b3a91c064c51ae50d57611bb6bb2260
Vyacheslav Kovalenko <vjacheslavkovalenko@gmail.com>
2023-12-14 14:04:31 +0300

    Commit N8. If dont work - DELETE this Commit!!!

--- app/src/main/java/by/icleborobot_repair/MainActivity.kt
+++ app/src/main/java/by/icleborobot_repair/MainActivity.kt
@@ -1,5 +1,6 @@
 package by.icleborobot_repair
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -7,17 +8,37 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+//нижеидущую строчку добавил:
+    lateinit var webView:WebView
 
-
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var urlWeb = getString(R.string.website_url)
-        val webView = findViewById<WebView>(R.id.xml_webview)
+        val urlWeb = getString(R.string.website_url)
+        //изменил это на нижеидущее:
+        //val webView = findViewById<WebView>(R.id.xml_webview)
+        webView = findViewById<WebView>(R.id.xml_webview)
+
 
         webView.webViewClient = WebViewClient()
         webView.loadUrl(urlWeb)
+
+        val webSettings=webView.settings
+
+        webSettings.javaScriptEnabled=true
+
     }
 
+    //нижеидущее добавил (всё):
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+    override fun onBackPressed() {
+        if (webView!!.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
 }
